cf2905924a004a3bd0d1e257b110f2d5eafed789
boyska <boyska@riseup.net>
2021-07-22 13:29:40 +0200

    DRAFT: display time in local timezone
    refs #6284

---
+++ config/chroot_local-includes/usr/share/gnome-shell/extensions/date@tails.boum.org/extension.js
@@ -0,0 +1,60 @@
+const Main = imports.ui.main;
+const GLib = imports.gi.GLib;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+
+var settings;
+
+function init() {
+}
+
+function overrider(lbl) {
+    // var FORMAT = settings.get_string("override-string");
+    // var now = GLib.DateTime.new_now_local();
+
+    // var desired = Format.format(FORMAT, now);
+    var now = new Date()
+
+    // var desired = Math.random().toString()
+    var desired = now.toLocaleString('en-US', {timeZone: 'Asia/Shanghai'})
+
+    var t = lbl.get_text();
+    if (t != desired) {
+        last = t;
+        lbl.set_text(desired);
+    }
+}
+
+var lbl, signalHandlerID, last = "";
+
+function enable() {
+    var sA = Main.panel.statusArea;
+    if (!sA) { sA = Main.panel._statusArea; }
+
+    if (!sA || !sA.dateMenu || !sA.dateMenu.actor) {
+        print("Looks like Shell has changed where things live again; aborting.");
+        return;
+    }
+
+    sA.dateMenu.actor.first_child.get_children().forEach(function(w) {
+        // assume that the text label is the first StLabel we find.
+        // This is dodgy behaviour but there's no reliable way to
+        // work out which it is.
+        w.set_style("text-align: center;");
+        if (w.get_text && !lbl) { lbl = w; }
+    });
+    if (!lbl) {
+        print("Looks like Shell has changed where things live again; aborting.");
+        return;
+    }
+    signalHandlerID = lbl.connect("notify::text", overrider);
+    last = lbl.get_text();
+    overrider(lbl);
+}
+
+function disable() {
+    if (lbl && signalHandlerID) {
+        lbl.disconnect(signalHandlerID);
+        lbl.set_text(last);
+    }
+}
