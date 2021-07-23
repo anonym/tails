ef3c9e97e3b13af29e7bc8c5629967c8eba72047
anonym <anonym@riseup.net>
2021-07-23 13:43:20 +0200

    Test suite: test that tor's control port is open for real.
    Apparently there are times when tor is running and listening on the
    control port but still not ready for connections to it. So let's just
    wait until some control port command actually succeeds.

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -470,13 +470,10 @@ When /^I see the "(.+)" notification(?: after at most (\d+) seconds)?$/ do |titl
 end
 
 Given /^Tor is ready$/ do
-  # First we wait for tor to be running and its control port to be open...
+  # First we wait for tor's control port to be ready...
   try_for(60) do
-    $vm.execute_successfully('systemctl -q is-active tor@default.service')
-    $vm.execute('ss -tlpn').stdout.chomp.split("\n").any? do |line|
-      _, _, _, listen_ip_port, _, user_pid, _ = line.split
-      listen_ip_port == '127.0.0.1:9052' && !user_pid['users:(("tor"'].nil?
-    end
+    $vm.execute_successfully('tor_control_getinfo version', libs: 'tor')
+    true
   end
   # ... so we can ask if the tor's networking is disabled, in which
   # case Tor Connection Assistant has not been dealt with yet. If
