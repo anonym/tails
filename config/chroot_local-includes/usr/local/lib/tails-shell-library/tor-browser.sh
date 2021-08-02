anonym/tails

commit 9c74ee223e078aff884e83c1c39bb9ba6c420ff8

Remove unused exec_unconfined_firefox().

config/chroot_local-includes/usr/local/lib/tails-shell-library/tor-browser.sh
@@ -19,8 +19,6 @@ set_mozilla_pref() {
 }
 
 exec_firefox_helper() {
-    local binary="${1}"; shift
-
     export LD_LIBRARY_PATH="${TBB_INSTALL}"
     export FONTCONFIG_PATH="${TBB_INSTALL}/TorBrowser/Data/fontconfig"
     export FONTCONFIG_FILE="fonts.conf"
@@ -47,15 +45,7 @@ exec_firefox_helper() {
     # From start-tor-browser:
     unset SESSION_MANAGER
 
-    exec "${TBB_INSTALL}"/"${binary}" "${@}"
-}
-
-exec_firefox() {
-    exec_firefox_helper firefox.real "${@}"
-}
-
-exec_unconfined_firefox() {
-    exec_firefox_helper firefox-unconfined "${@}"
+    exec "${TBB_INSTALL}"/firefox.real "${@}"
 }
 
 guess_best_tor_browser_locale() {

config/chroot_local-hooks/10-tbb
@@ -333,10 +333,6 @@ rmdir "${TBB_INSTALL}"/TorBrowser/Data/Browser/profile.default/extensions
 mkdir -p "${TBB_PROFILE}"
 create_default_profile "${TBB_INSTALL}"/TorBrowser/Data/Browser/profile.default "${TBB_EXT}" "${TBB_PROFILE}"
 
-# Create a copy of the Firefox binary, for use e.g. by the Unsafe Browser.
-# It won't be subject to AppArmor confinement.
-cp -a "${TBB_INSTALL}/firefox.real" "${TBB_INSTALL}/firefox-unconfined"
-
 chown -R root:root "${TBB_INSTALL}" "${TBB_PROFILE}" "${TBB_EXT}"
 chmod -R a+rX "${TBB_INSTALL}" "${TBB_PROFILE}" "${TBB_EXT}"
 

commit 63aeb9a6b507fe22662bc76348f946755278ebb9

Give function the name we use
Fixes regression introduced by refactoring
in 9c74ee223e078aff884e83c1c39bb9ba6c420ff8.

config/chroot_local-includes/usr/local/lib/tails-shell-library/tor-browser.sh
@@ -18,7 +18,7 @@ set_mozilla_pref() {
     echo "${prefix}(\"${name}\", ${value});" >> "${file}"
 }
 
-exec_firefox_helper() {
+exec_firefox() {
     export LD_LIBRARY_PATH="${TBB_INSTALL}"
     export FONTCONFIG_PATH="${TBB_INSTALL}/TorBrowser/Data/fontconfig"
     export FONTCONFIG_FILE="fonts.conf"