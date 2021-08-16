31178f320c87bb037d8fbd956e92d8d6d4e26482
intrigeri <intrigeri@boum.org>
2021-08-16 18:40:48 +0000

    Test suite: prepare sandbox test for when #18470 is fixed
    Given the current implementation of the test about tor seccomp sandbox, there's
    no simple way to make "Tor is ready" pass reliably in the "TCA can reconnect
    after a connection failure" scenario.
    
    But once #18470 is fixed, this should be straightforward, so here
    I'm adding the code we'll need once we're there.

--- features/step_definitions/tor.rb
+++ features/step_definitions/tor.rb
@@ -697,11 +697,20 @@ Given /^the Tor network( and default bridges)? (?:is|are) (un)?blocked$/ do |def
       '/lib/live/mount/rootfs/filesystem.squashfs/etc/NetworkManager/dispatcher.d/00-firewall.sh ' \
       '/etc/NetworkManager/dispatcher.d/00-firewall.sh'
     )
-  else
-    # Tor Connection will fallback to default (obfs4) bridges if
-    # access to the Tor network is blocked
-    @tor_is_using_pluggable_transports = true
   end
+  @tor_is_using_pluggable_transports =
+    # XXX: uncomment once #18470 is fixed: currently the sandbox may
+    # or may not be enabled, depending on whether we end up connecting
+    # directly or with the default bridges.
+    # if unblock
+    #   # Undo what we did in the "else" clause while blocking tor:
+    #   # once unblocked, we're going to use a direct connection.
+    #   true
+    # else
+      # Tor Connection will fallback to default (obfs4) bridges if
+      # access to the Tor network is blocked
+      true
+    # end
 end
 
 Then /^Tor is configured to use the default bridges$/ do
