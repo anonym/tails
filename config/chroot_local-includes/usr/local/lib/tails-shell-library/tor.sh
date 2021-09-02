519926fd5bca11ccc8c4f435aae790f0e40e420a
anonym <anonym@riseup.net>
2021-09-02 17:14:29 +0200

    Avoid spamming new python interpreters while waiting for Tor to bootstrap.
    Now that the tor.sh shell library is based on python-stem, each call
    spawns a python interpreter, which is pretty heavy. Especially when
    spamming tor_is_working() like when waiting for Tor to bootstrap. So
    we provide tor_wait_until_bootstrapped() which does all of it in once
    instance of python interpreter.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/tor.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/tor.sh
@@ -58,9 +58,31 @@ tor_bootstrap_progress() {
        echo "${res:-0}"
 }
 
+# Only use this if you truly want a one-off check, otherwise consider
+# using tor_wait_until_bootstrapped() which avoids firing up a new
+# python interpreter for each check.
 tor_is_working() {
 	[ "$(tor_bootstrap_progress)" -eq 100 ] && \
 	    [ "$(tor_control_getinfo status/enough-dir-info)" -eq 1 ]
+}
+
+tor_wait_until_bootstrapped() {
+    local timeout
+    timeout="${1:-0}"
+    tor_control_stem_wrapper "
+import time
+stop_time = time.time() + ${timeout}
+while ${timeout} <= 0 or time.time() < stop_time:
+    try:
+        progress = controller.get_info('status/bootstrap-phase').split()[2].split('=')[1]
+    except ValueError:
+        progress = 0
+    enough_dir_info = controller.get_info('status/enough-dir-info')
+    if enough_dir_info == '1' and progress == '100':
+        exit(0)
+    time.sleep(1)
+exit(1)
+"
 }
 
 tor_append_to_torrc () {
