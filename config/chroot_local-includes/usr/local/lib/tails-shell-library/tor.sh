e840d9af730befdb3532e0c8ca5b181001757b81
anonym <anonym@riseup.net>
2021-11-08 15:42:25 +0100

    Fix indentation.

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/tor.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/tor.sh
@@ -8,16 +8,16 @@ TOR_LOG=/var/log/tor/log
 TOR_DIR=/var/lib/tor
 
 tor_rc_lookup() {
-	grep --no-filename "^${1}\s" "${TOR_RC_DEFAULTS}" "${TOR_RC}" | \
-	    sed --regexp-extended "s/^${1}\s+(.+)$/\1/" | tail -n1
+    grep --no-filename "^${1}\s" "${TOR_RC_DEFAULTS}" "${TOR_RC}" | \
+	sed --regexp-extended "s/^${1}\s+(.+)$/\1/" | tail -n1
 }
 
 tor_control_port() {
-	tor_rc_lookup ControlPort | sed --regexp-extended 's/.*://'
+    tor_rc_lookup ControlPort | sed --regexp-extended 's/.*://'
 }
 
 tor_control_stem_wrapper() {
-        python3 <<EOF
+    python3 <<EOF
 import stem
 import stem.connection
 import sys
@@ -41,34 +41,34 @@ EOF
 }
 
 tor_control_getinfo() {
-	tor_control_stem_wrapper "print(controller.get_info('${1}'))"
+    tor_control_stem_wrapper "print(controller.get_info('${1}'))"
 }
 
 tor_control_getconf() {
-	tor_control_stem_wrapper "print(controller.get_conf('${1}'))"
+    tor_control_stem_wrapper "print(controller.get_conf('${1}'))"
 }
 
 tor_control_setconf() {
-	tor_control_stem_wrapper "controller.set_conf('${1}', '${2}')"
+    tor_control_stem_wrapper "controller.set_conf('${1}', '${2}')"
 }
 
 tor_bootstrap_progress() {
-       local res
-       res=$(tor_control_getinfo status/bootstrap-phase | \
-                    sed --regexp-extended 's/^.* BOOTSTRAP PROGRESS=([[:digit:]]+) .*$/\1/')
-       echo "${res:-0}"
+    local res
+    res=$(tor_control_getinfo status/bootstrap-phase | \
+              sed --regexp-extended 's/^.* BOOTSTRAP PROGRESS=([[:digit:]]+) .*$/\1/')
+    echo "${res:-0}"
 }
 
 # Only use this if you truly want a one-off check, otherwise consider
 # /usr/local/lib/tor_wait_until_bootstrapped which avoids firing up a
 # new python interpreter for each check.
 tor_is_working() {
-	[ "$(tor_bootstrap_progress)" -eq 100 ] && \
-	    [ "$(tor_control_getinfo status/enough-dir-info)" -eq 1 ]
+    [ "$(tor_bootstrap_progress)" -eq 100 ] && \
+	[ "$(tor_control_getinfo status/enough-dir-info)" -eq 1 ]
 }
 
 tor_append_to_torrc () {
-	echo "${@}" >> "${TOR_RC}"
+    echo "${@}" >> "${TOR_RC}"
 }
 
 # Set a (possibly existing) option $1 to $2 in torrc. Shouldn't be
@@ -76,6 +76,6 @@ tor_append_to_torrc () {
 # options). Does not support configuration entries split into multiple
 # lines (with the backslash character).
 tor_set_in_torrc () {
-	sed -i "/^${1}\s/d" "${TOR_RC}"
-	tor_append_to_torrc "${1} ${2}"
+    sed -i "/^${1}\s/d" "${TOR_RC}"
+    tor_append_to_torrc "${1} ${2}"
 }
