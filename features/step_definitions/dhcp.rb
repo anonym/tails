anonym/tails

commit 4e45a449c3b2c16a8ce61c8e12afc2373b2e5e28

Test suite refactoring: imperative → functional style

features/step_definitions/dhcp.rb
@@ -7,13 +7,13 @@ Then /^the hostname should not have been leaked on the network$/ do
     packets = PacketFu::PcapFile.new.file_to_array(filename: @sniffer.pcap_file)
     expected_types = [PacketFu::IPv6Packet, PacketFu::IPPacket,
                       PacketFu::ARPPacket,]
-    packets.each do |packet|
-      type = expected_types.find { |t| t.can_parse?(packet) }
-      assert_not_nil(type, 'Found non-IP(v6)/ARP packet')
-      payload = type.parse(packet).payload
-      hostnames.each do |hostname|
-        raise DhcpLeakError, "Hostname leak detected: #{hostname}" if payload.match(hostname)
-      end
+    if packets.any? do |packet|
+         type = expected_types.find { |t| t.can_parse?(packet) }
+         assert_not_nil(type, 'Found non-IP(v6)/ARP packet')
+         payload = type.parse(packet).payload
+         hostnames.any? { |hostname| payload.match(hostname) }
+       end
+      raise DhcpLeakError, "Hostname leak detected: #{hostname}" \
     end
   rescue DhcpLeakError => e
     save_failure_artifact('Network capture', @sniffer.pcap_file)

commit 312ed286e11cf93fdd998588fddedacaa4a2bb5d

Test suite: fix reporting of hostname leaks via DHCP
This problem can't be reproduced on Tails 4.x, which does not leak
the hostname. But on feature/bullseye we see this:

  undefined local variable or method `hostname' for #<Object:0x00005623d262c9a0> (NameError)

Fixes regression I introduced in 4e45a449c3b2c16a8ce61c8e12afc2373b2e5e28.

features/step_definitions/dhcp.rb
@@ -12,7 +12,7 @@ Then /^the hostname should not have been leaked on the network$/ do
        payload = type.parse(packet).payload
        hostnames.any? { |hostname| payload.match(hostname) }
      end
-    raise DhcpLeakError, "Hostname leak detected: #{hostname}" \
+    raise DhcpLeakError, 'Hostname leak detected'
   end
 rescue DhcpLeakError => e
   save_failure_artifact('Network capture', @sniffer.pcap_file)