36ae6ef7c3393dd6993cbadff4f3458cf8d92156
boyska <boyska@riseup.net>
2021-11-18 16:09:49 +0100

    race conditions? let's increase sleep time!
    yes, not ideal, I know

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tails_installer/creator.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tails_installer/creator.py
@@ -1083,7 +1083,7 @@ class TailsInstallerCreator(object):
     def rescan_block_device(self, block):
         self._udisksclient.settle()
         self.flush_buffers(silent=True)
-        time.sleep(5)
+        time.sleep(30)
         block.call_rescan_sync(GLib.Variant('a{sv}', None))
 
     def connect_drive_monitor(self, callback, data=None):
