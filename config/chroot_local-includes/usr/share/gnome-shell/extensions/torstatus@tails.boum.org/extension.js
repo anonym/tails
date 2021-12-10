0885d4639c9250bdd3c8ddad9f3de143597e2a4c
anonym <anonym@riseup.net>
2021-12-10 15:04:14 +0100

    Port torstatus GNOME shell extension to Bullseye.
    Close: #18703

--- config/chroot_local-includes/usr/share/gnome-shell/extensions/torstatus@tails.boum.org/extension.js
+++ config/chroot_local-includes/usr/share/gnome-shell/extensions/torstatus@tails.boum.org/extension.js
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **********************************************************************/
 
 const Gio = imports.gi.Gio;
+const GObject = imports.gi.GObject;
 const Shell = imports.gi.Shell;
 const St = imports.gi.St;
 
@@ -30,9 +31,10 @@ const _ = Gettext.gettext;
 const TorStatusIndicatorName = 'tor-status';
 const TorStatusIndicatorStatusFile = '/run/tor-has-bootstrapped/done';
 
+var TorStatusIndicator = GObject.registerClass(
 class TorStatusIndicator extends PanelMenu.Button {
-    constructor() {
-        super(0.0, _("Tor Status"));
+    _init() {
+        super._init(0.0, _("Tor Status"));
 
         // Monitor the status file
         let status_file = Gio.File.new_for_path(TorStatusIndicatorStatusFile);
@@ -43,8 +45,8 @@ class TorStatusIndicator extends PanelMenu.Button {
         // Create icon
         this._icon = new St.Icon({ style_class: 'system-status-icon' });
         this._updateIcon(status_file.query_exists(null));
-        this.actor.add_actor(this._icon);
-        this.actor.add_style_class_name('panel-status-button');
+        this.add_actor(this._icon);
+        this.add_style_class_name('panel-status-button');
 
         // Create menu
         let menu_item = new PopupMenu.PopupMenuItem(_("Open Onion Circuits"));
@@ -85,7 +87,7 @@ class TorStatusIndicator extends PanelMenu.Button {
         super.destroy();
         log("TorStatus: exiting destroy()");
     }
-};
+});
 
 let tor_status_indicator;
 
