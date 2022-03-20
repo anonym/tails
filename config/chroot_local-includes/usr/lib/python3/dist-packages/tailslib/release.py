54b731a2f25e64f177c3cd11979a59bfad9b2f05
boyska <boyska@riseup.net>
2022-03-20 14:59:54 +0100

    "date in past" check is more robust
     - correct exit code
     - timezone

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/release.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/release.py
@@ -10,5 +10,7 @@ def get_release_date() -> datetime.datetime:
     with open('/etc/amnesia/version') as buf:
         firstline = next(iter(buf))
     _, source_date = firstline.split(' - ')
+    source_date = source_date.strip()
     source_dt = datetime.datetime.strptime(source_date, '%Y%m%d')
+    source_dt = source_dt.replace(tzinfo=datetime.timezone.utc)
     return source_dt
