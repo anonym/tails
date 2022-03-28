f8c2feae7e36d63d1678ba75d4a0bf0758df8442
intrigeri <intrigeri@boum.org>
2022-03-28 20:17:18 +0000

    Lint: gofmt

--- config/chroot_local-includes/usr/src/https-get-expired.go
+++ config/chroot_local-includes/usr/src/https-get-expired.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    // "log"
+	// "log"
 	"crypto/tls"
 	"crypto/x509"
-    "flag"
+	"flag"
 	"fmt"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
-    "time"
+	"time"
 )
 
 var verifyOpts x509.VerifyOptions
@@ -20,36 +20,36 @@ var now = time.Now()
 var rejectExpired = false
 
 func init() {
-    var err error
-    onesecond_more, err = time.ParseDuration("1s")
-    if err != nil { // but this should never happen
-        panic(err)
-    }
+	var err error
+	onesecond_more, err = time.ParseDuration("1s")
+	if err != nil { // but this should never happen
+		panic(err)
+	}
 }
 
 func verifyButAcceptExpired(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
-    certs := make([]*x509.Certificate, len(rawCerts))
+	certs := make([]*x509.Certificate, len(rawCerts))
 	for i, asn1Data := range rawCerts {
 		cert, _ := x509.ParseCertificate(asn1Data)
-        certs[i] = cert
+		certs[i] = cert
 	}
-    for _, cert := range certs[1:] {
-        verifyOpts.Intermediates.AddCert(cert)
-    }
+	for _, cert := range certs[1:] {
+		verifyOpts.Intermediates.AddCert(cert)
+	}
 
 	if err := certs[0].VerifyHostname(verifyOpts.DNSName); err != nil {
-        return err
-    }
+		return err
+	}
 
-    // log.Println("shall we reject?", rejectExpired, "is future?", certs[0].NotBefore.After(currentTime))
-    if !rejectExpired || certs[0].NotBefore.After(currentTime) {
-        verifyOpts.CurrentTime = certs[0].NotBefore.Add(onesecond_more)
-    } else {
-        verifyOpts.CurrentTime = currentTime
-    }
+	// log.Println("shall we reject?", rejectExpired, "is future?", certs[0].NotBefore.After(currentTime))
+	if !rejectExpired || certs[0].NotBefore.After(currentTime) {
+		verifyOpts.CurrentTime = certs[0].NotBefore.Add(onesecond_more)
+	} else {
+		verifyOpts.CurrentTime = currentTime
+	}
 
 	if _, err := certs[0].Verify(verifyOpts); err != nil {
-        return err
+		return err
 	}
 
 	return nil
@@ -58,26 +58,25 @@ func verifyButAcceptExpired(rawCerts [][]byte, verifiedChains [][]*x509.Certific
 // XXX: emulate htpdate --proxy, that is curl --socks5-hostname
 
 func main() {
-    var err error
-    flag.BoolVar(&rejectExpired, "reject-expired", false, "If set, only future certificates are accepted.")
+	var err error
+	flag.BoolVar(&rejectExpired, "reject-expired", false, "If set, only future certificates are accepted.")
 	user_agent := flag.String("user-agent", "", "Set user-agent header.")
-	timeout := flag.Duration("timeout", 30 * time.Second, "Request timeout.")
-    proxy := flag.String("proxy", "", "Set a proxy for the request. socks5:// syntax supported")
+	timeout := flag.Duration("timeout", 30*time.Second, "Request timeout.")
+	proxy := flag.String("proxy", "", "Set a proxy for the request. socks5:// syntax supported")
 
 	output_headers := flag.String("output", "", "Write date header to FILE. If omitted, date is printed on stdout.")
 
-    currentTimeS := flag.String("current-time", "", "simulate a different current-time. Debug only!")
-    flag.Parse()
-    if len(*currentTimeS) > 0 {
-        currentTime, err = time.Parse("2006-01-02", *currentTimeS)
-        if err != nil {
-            fmt.Fprintln(os.Stderr, "Invalid format for current-time:", err.Error())
-            os.Exit(2)
-        }
-    } else {
-        currentTime = time.Now()
-    }
-
+	currentTimeS := flag.String("current-time", "", "simulate a different current-time. Debug only!")
+	flag.Parse()
+	if len(*currentTimeS) > 0 {
+		currentTime, err = time.Parse("2006-01-02", *currentTimeS)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Invalid format for current-time:", err.Error())
+			os.Exit(2)
+		}
+	} else {
+		currentTime = time.Now()
+	}
 
 	urlString := flag.Args()[0]
 	urlRequest, err := url.Parse(urlString)
@@ -86,23 +85,23 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(2)
 	}
-    hostname := urlRequest.Hostname()
+	hostname := urlRequest.Hostname()
 
-    config := tls.Config{VerifyPeerCertificate: verifyButAcceptExpired, InsecureSkipVerify: true, MinVersion: tls.VersionTLS10}
+	config := tls.Config{VerifyPeerCertificate: verifyButAcceptExpired, InsecureSkipVerify: true, MinVersion: tls.VersionTLS10}
 	transCfg := &http.Transport{
 		TLSClientConfig: &config,
-        Proxy: func(req *http.Request) (*url.URL, error) {
-            if *proxy == "" {
-                return nil, nil
-            }
-            return url.Parse(*proxy)
-        },
+		Proxy: func(req *http.Request) (*url.URL, error) {
+			if *proxy == "" {
+				return nil, nil
+			}
+			return url.Parse(*proxy)
+		},
 	}
-    verifyOpts = x509.VerifyOptions{
-        Roots:         config.RootCAs,
-        DNSName:       hostname,
-        Intermediates: x509.NewCertPool(),
-    }
+	verifyOpts = x509.VerifyOptions{
+		Roots:         config.RootCAs,
+		DNSName:       hostname,
+		Intermediates: x509.NewCertPool(),
+	}
 	client := &http.Client{Transport: transCfg, Timeout: *timeout}
 
 	request, err := http.NewRequest("HEAD", urlString, nil)
@@ -124,13 +123,13 @@ func main() {
 
 		// don't output headers we don't care about
 		exclude_headers := make(map[string]bool)
-		for key, _ := range(response.Header) {
+		for key := range response.Header {
 			if strings.ToLower(key) != "date" {
 				exclude_headers[key] = true
 			}
 		}
 
-		buf, err := os.OpenFile(*output_headers, os.O_WRONLY | os.O_CREATE, 0600)
+		buf, err := os.OpenFile(*output_headers, os.O_WRONLY|os.O_CREATE, 0600)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -146,5 +145,5 @@ func main() {
 		fmt.Println(response.Header.Get("date"))
 	}
 
-    os.Exit(0)
+	os.Exit(0)
 }
