50e950370157e0f4dceb9b1f0048f6b580dfd330
intrigeri <intrigeri@boum.org>
2022-04-19 09:32:48 +0000

    Test suite: migrate from deprecated luks_open and luks_close to cryptsetup_{open,close}
    This removes some deprecation warnings in the test suite output.
    We can do this now that we assume Bullseye or newer on systems
    that run the test suite.
    
    refs #18837

--- features/step_definitions/usb.rb
+++ features/step_definitions/usb.rb
@@ -729,7 +729,7 @@ Then /^only the expected files are present on the persistence partition on USB d
     assert_not_nil(partition, "Could not find the 'TailsData' partition " \
                               "on disk '#{disk_handle}'")
     luks_mapping = File.basename(partition) + '_unlocked'
-    g.luks_open(partition, @persistence_password, luks_mapping)
+    g.cryptsetup_open(partition, @persistence_password, luks_mapping)
     luks_dev = "/dev/mapper/#{luks_mapping}"
     mount_point = '/'
     g.mount(luks_dev, mount_point)
@@ -747,7 +747,7 @@ Then /^only the expected files are present on the persistence partition on USB d
       )
     end
     g.umount(mount_point)
-    g.luks_close(luks_dev)
+    g.cryptsetup_close(luks_dev)
   end
 end
 
