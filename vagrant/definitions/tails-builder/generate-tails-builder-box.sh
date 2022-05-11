e34a090264b6331945a974206773ac19623a0c4c
Toptin <toptin@riseup.net>
2022-05-11 15:07:09 +0100

    Upgrade apt-cacher-ng to bullseye-backports.
    (cherry picked from commit 70574517a46006a9adead0f6e0077df4003d7bf3)

--- vagrant/definitions/tails-builder/generate-tails-builder-box.sh
+++ vagrant/definitions/tails-builder/generate-tails-builder-box.sh
@@ -127,6 +127,11 @@ steps:
       sed -e 's/${DISTRIBUTION}/${DISTRIBUTION}-updates/' /etc/apt/sources.list \\
         > "/etc/apt/sources.list.d/${DISTRIBUTION}-updates.list"
 
+  - chroot: rootfs
+    shell: |
+      sed -e 's/${DISTRIBUTION}/${DISTRIBUTION}-backports/' /etc/apt/sources.list \\
+        > "/etc/apt/sources.list.d/${DISTRIBUTION}-backports.list"
+
   - create-file: /etc/apt/sources.list.d/${DISTRIBUTION}-security.list
     contents: |
       deb http://time-based.snapshots.deb.tails.boum.org/debian-security/${DEBIAN_SECURITY_SERIAL}/ ${DISTRIBUTION}-security main
@@ -168,7 +173,7 @@ steps:
 
   - apt: install
     packages:
-      - apt-cacher-ng
+      - apt-cacher-ng/${DISTRIBUTION}-backports
       - ca-certificates
       - curl
       # Install dbus to ensure we can use timedatectl
