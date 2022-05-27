anonym/tails

commit 67f9850e68fe57d66ec26c8e69e70f08501661e2

Test suite: DRY
refs #18547

features/step_definitions/tor.rb
@@ -380,10 +380,7 @@ Then /^the Tor Connection Assistant connects to Tor$/ do
       done = true
     else
       done = tor_connection_assistant.child?(
-        'Connected to Tor successfully', roleName: 'label',
-        retry: false, showingOnly: true
-      ) || tor_connection_assistant.child?(
-        'Connected to Tor successfully (using bridges)', roleName: 'label',
+        /\AConnected to Tor successfully\b/, roleName: 'label',
         retry: false, showingOnly: true
       )
     end

commit 53751873346c0490e5f6b69961019faf65123bfa

Test suite: fix regression when testing Tor Connection in non-English locale
This reverts commit 67f9850e68fe57d66ec26c8e69e70f08501661e2,
which broke "Tails is localized for every tier-1 language":
matching using regexps does not support localization of strings.

Also, update the expected string when using bridges to match the new
one: 37e2fa76c1d051269b027b40cf8a0ea08b10aa0b.

features/step_definitions/tor.rb
@@ -380,7 +380,10 @@ Then /^the Tor Connection Assistant connects to Tor$/ do
       done = true
     else
       done = tor_connection_assistant.child?(
-        /\AConnected to Tor successfully\b/, roleName: 'label',
+        'Connected to Tor successfully', roleName: 'label',
+        retry: false, showingOnly: true
+      ) || tor_connection_assistant.child?(
+        'Connected to Tor successfully with bridges', roleName: 'label',
         retry: false, showingOnly: true
       )
     end