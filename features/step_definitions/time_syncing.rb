3841d57240d711f355dd74d67a0ef5563a78854f
boyska <boyska@riseup.net>
2022-05-30 10:08:24 +0200

    FIX error message
    the previous one was just spitting IDs, which is totally unhelpful

--- features/step_definitions/time_syncing.rb
+++ features/step_definitions/time_syncing.rb
@@ -95,10 +95,11 @@ def displayed_time_str
     Dogtail::Application.new('gnome-shell')
                         .child('', roleName: 'panel')
                         .children(showingOnly: true, roleName: 'label')
-  ).keep_if { |l| !ignore_labels.include?(l.name) }
+                        .map(&:name)
+  ).keep_if { |l| !ignore_labels.include?(l) }.to_a
 
   assert_equal(1, candidate_clock_labels.size, "Too many candidate_clock_labels: #{candidate_clock_labels}")
-  candidate_clock_labels.to_a[0].name
+  candidate_clock_labels[0]
 end
 
 Then /^the displayed clock is less than (\d+) minutes incorrect in "([^"]*)"/ do |max_diff_mins, timezone_offset|
