9f0d5f3343d47f5aed14a269b33c65e369c7528a
intrigeri <intrigeri@boum.org>
2022-08-03 16:22:06 +0000

    Lint

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tps_frontend/passphrase_strength_hint.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tps_frontend/passphrase_strength_hint.py
@@ -3,6 +3,7 @@ from gi.repository import Gtk
 
 from tps_frontend import _
 
+
 def set_passphrase_strength_hint(progress_bar: Gtk.ProgressBar,
                                  passhrase: str):
     def get_passphrase_strength() -> float:
