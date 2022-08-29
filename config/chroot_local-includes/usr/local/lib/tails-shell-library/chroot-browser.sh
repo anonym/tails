1aa933bd3f0b2fc1ae3424aea0c6e9e2df7e0e6b
intrigeri <intrigeri@boum.org>
2022-08-29 16:29:46 +0000

    Disable shellcheck false positive

--- config/chroot_local-includes/usr/local/lib/tails-shell-library/chroot-browser.sh
+++ config/chroot_local-includes/usr/local/lib/tails-shell-library/chroot-browser.sh
@@ -45,6 +45,7 @@ setup_chroot_for_browser () {
     # for creating isolated jails, the chroot can be used as its rootfs.
 
     local cleanup_cmd="try_cleanup_browser_chroot \"${chroot}\" \"${cow}\""
+    # shellcheck disable=SC2064
     trap "${cleanup_cmd}" INT EXIT
 
     local rootfs_dir
