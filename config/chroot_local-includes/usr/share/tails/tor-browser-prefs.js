0b2bd214511ecc7a4398f4c16dcb8b58d839f44f
intrigeri <intrigeri@boum.org>
2022-10-31 10:22:33 +0000

    Tor Browser: remove network.security.ports.banned pref, which does more harm than good
    This change was applied upstream:
    https://gitlab.torproject.org/tpo/applications/tor-browser/-/merge_requests/424
    
    This pref was:
    
     - useless, because connections to localhost are already blocked by
       setting `network.proxy.allow_hijacking_localhost` to true
     - harmful, because it allowed fingerprinting Tor Browser users…
       and even more so, distinguishing users of Tor Browser in Tails
       from users of pristine Tor Browser
    
    refs tails/tails#19085

--- config/chroot_local-includes/usr/share/tails/tor-browser-prefs.js
+++ config/chroot_local-includes/usr/share/tails/tor-browser-prefs.js
@@ -1,9 +1,3 @@
-// As suggested in TBB's start-browser script for system-wide Tor
-// instances.
-// Warning: when updating this, also update
-// config/chroot_local-includes/etc/thunderbird/pref/thunderbird.js
-pref("network.security.ports.banned", "631,6136,4444,4445,6668,7656,7657,7658,7659,7660,8998,9040,9050,9062,9150,9051");
-
 // Tails-specific configuration below
 
 // Since the slider notification will be shown everytime at each Tails
