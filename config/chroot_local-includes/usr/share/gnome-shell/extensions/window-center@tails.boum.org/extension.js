59e37ce5a7074256f06886b38373612ec8bc593d
boyska <boyska@riseup.net>
2022-12-07 18:37:36 +0100

    avoid useless CPU work

--- config/chroot_local-includes/usr/share/gnome-shell/extensions/window-center@tails.boum.org/extension.js
+++ config/chroot_local-includes/usr/share/gnome-shell/extensions/window-center@tails.boum.org/extension.js
@@ -48,7 +48,9 @@ function centerEveryWindow() {
         window.move_resize_frame(true, x, y, rect.width, rect.height);
         rect = window.get_frame_rect()
         global.log(`${EXTENSION_LOG_NAME} moved: ${rect.x}-${rect.y}+${rect.width}x${rect.height}`);
+        return true;
     }
+    return false;
 }
 
 let _interval;
@@ -59,9 +61,6 @@ function init() {
 
 function enable() {
     centerEveryWindow();
-    // XXX: this timer is incredibly fast, because that's the easiest way to have a snappy UI
-    // as a "mitigation" for the high CPU cost, we only run it a few times
-    // a better solution would be stopping as soon as the only relevant window has been found and moved
     let intervalMS = 50;
     let totalTime = 3000;
     let refreshCount=totalTime / intervalMS;
@@ -69,9 +68,11 @@ function enable() {
         intervalMS, /* milliseconds */
         () => {
             refreshCount-=1;
-            centerEveryWindow();
-            if (refreshCount<=0) return GLib.SOURCE_REMOVE;
-            else return GLib.SOURCE_CONTINUE;
+            if (centerEveryWindow() || refreshCount < 0) {
+                global.log(`${EXTENSION_LOG_NAME} leaving`);
+                return GLib.SOURCE_REMOVE;
+            }
+            return GLib.SOURCE_CONTINUE;
         });
 }
 
